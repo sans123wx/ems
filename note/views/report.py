c03ee804f966f21656abb0a6b8925c47ad83123e
sans123wx <514232900@qq.com>
2019-02-27 22:47:45 +0800

    报账部门列表

--- note/views/report.py
+++ note/views/report.py
@@ -1,27 +1,29 @@
 from ..models import *
+from django.shortcuts import render
 
 def get_info(user , groups):
 	dict_permissions = {'电教部':user.permissions.bz_dj , '网络信息部':user.permissions.bz_wl , '计算机部':user.permissions.bz_jsj}
-	list_info = {}
+	list_groups = []
 	for group in groups:
 		if dict_permissions[group.name]:
-			customers = Customer.objects.filter(group = groups)
-			list_info[group] = customers
-	notes = notes = Note.objects.filter(sh = list_info[groups[0]][0])
-	return list_info , notes
+			customers = Customer.objects.filter(group = group)
+			list_groups.append(group)
+	notes = Note.objects.filter(sh__group = list_groups[0])
+	return list_groups , notes
 
 def bz(request):
 	user = request.user
 	groups = user.groups.all()
-	if (user.permission.bz_dj or user.permission.bz_wl or user.permission.bz_jsj) and groups:
+	list_groups = []
+	if (user.permissions.bz_dj or user.permissions.bz_wl or user.permissions.bz_jsj) and groups:
 		if len(groups) == 1:
 			customers = Customer.objects.filter(group = groups[0])
 			notes = Note.objects.filter(sh = customers[0])
-			list_info = {group[0] , customers}
+			list_groups.append(groups[0])
 		else:
-			list_info , notes = get_info(user , groups)
+			list_groups , notes = get_info(user , groups)
 		context = {}
-		context['list_info'] = list_info
+		context['list_groups'] = list_groups
 		context['notes'] = notes
 		return render(request , 'note/report/bz.html' , context)
 	else:
