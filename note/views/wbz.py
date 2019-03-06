7fd9134b78a6d31e9cbc96104266f5872ff2d7e4
sans123wx <514232900@qq.com>
2019-03-06 22:55:46 +0800

    highcharts图表

--- note/views/wbz.py
+++ note/views/wbz.py
@@ -14,6 +14,7 @@ def wbz_all(request):
 	context['total_p'] = total_p
 	return render(request , 'note/show_notes/wbz/wbz_all.html' , context)
 
+#电教部未报账记录
 def wbz_dj(request):
 	notes = Note.objects.filter(bz = False , sh__group__name = '电教部')
 	total_p = notes.aggregate(total_p = Sum('hj'))['total_p']
@@ -24,6 +25,7 @@ def wbz_dj(request):
 	context['total_p'] = total_p
 	return render(request , 'note/show_notes/wbz/wbz_dj.html' , context)
 
+#计算机部未报账记录
 def wbz_jsj(request):
 	notes = Note.objects.filter(bz = False , sh__group__name = '计算机部')
 	total_p = notes.aggregate(total_p = Sum('hj'))['total_p']
@@ -34,6 +36,7 @@ def wbz_jsj(request):
 	context['total_p'] = total_p
 	return render(request , 'note/show_notes/wbz/wbz_jsj.html' , context)
 
+#网络信息部未报账记录
 def wbz_wl(request):
 	notes = Note.objects.filter(bz = False , sh__group__name = '网络信息部')
 	total_p = notes.aggregate(total_p = Sum('hj'))['total_p']
@@ -44,10 +47,15 @@ def wbz_wl(request):
 	context['total_p'] = total_p
 	return render(request , 'note/show_notes/wbz/wbz_wl.html' , context)
 
+#图表信息
 def wbz_tb(request):
 	groups = Group.objects.filter(customer__note__bz = False).annotate(group_p = Sum('customer__note__hj'))
-	total_p_all = groups.aggregate(total_p = Sum('group_p'))['total_p']
+	unit_types_dj = Unit_type.objects.filter(sh__group__name = '电教部' , note__bz = False).annotate(ut_p = Sum('note__hj'))
+	unit_types_jsj = Unit_type.objects.filter(sh__group__name = '计算机部' , note__bz = False).annotate(ut_p = Sum('note__hj'))
+	unit_types_wl = Unit_type.objects.filter(sh__group__name = '网络信息部' , note__bz = False).annotate(ut_p = Sum('note__hj'))
 	context = {}
 	context['groups'] = groups
-	context['total_p'] = total_p_all
+	context['unit_types_dj'] = unit_types_dj
+	context['unit_types_jsj'] = unit_types_jsj
+	context['unit_types_wl'] = unit_types_wl
 	return render(request , 'note/show_notes/wbz/wbz_tb.html' , context)
