c656d7effd7d94b052e98fac9eb7a3c6f68432f0
sans123wx <514232900@qq.com>
2019-03-13 18:50:05 +0800

    图标

--- ems/settings/production.py
+++ ems/settings/production.py
@@ -1,9 +1,9 @@
 from .base import *
 import os
 
-SECRET_KEY = os.environ['SECRET_KEY']
-DEBUG = False
 
+DEBUG = False
+SECRET_KEY = os.environ['SECRET_KEY']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
